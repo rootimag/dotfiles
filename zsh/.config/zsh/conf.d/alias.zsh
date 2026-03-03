b566250c5d7eeb650d19d87e736258e667cc06eb
rootimag <rootimag@gmail.com>
2026-03-03 15:22:37 +0800

    feat: 修复格式

--- zsh/.config/zsh/conf.d/alias.zsh
+++ zsh/.config/zsh/conf.d/alias.zsh
@@ -1,7 +1,7 @@
-# 别名，方便调用
-alias so='echo "󱗘 Reloading Zsh Profile..." && source $HOME/.config/zsh/.zshrc && echo -e "\033[32m󰄬 Done!\033[0m"'
+alias re='echo "󱗘 Reloading Zsh Profile..." && source $HOME/.config/zsh/.zshrc && echo -e "\033[32m󰄬 Done!\033[0m"'
+alias hf='history | fzf | wl-copy'
+alias sr='sudo sing-box run -c'
 
-# 常用命令别名
 alias ls='lsd -l'
 alias ll='lsd -la'
 alias history='history 1'
@@ -11,7 +11,6 @@ alias remove='paru -Rns'
 alias search='paru -Ss'
 alias about='paru -Si'
 
-# 目录导航别名
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
