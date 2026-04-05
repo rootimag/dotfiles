6423369d76a6ecb854de2827e48aaf2fecc248a1
rootimag <rootimag@gmail.com>
2026-04-05 17:51:26 +0800

    feat: 优化 waybar

--- zsh/.config/zsh/conf.d/colors.zsh
+++ zsh/.config/zsh/conf.d/colors.zsh
@@ -1,6 +1,6 @@
 # matugen 配色文件
 
-export FZF_DEFAULT_OPTS="--color=fg:#f0dfd8,hl:#ffb690 \
---color=fg+:#d7c2b9,hl+:#ffb690 \
---color=info:#e6beab,prompt:#ffb690,pointer:#ffb690 \
---color=marker:#cfc890,spinner:#ffb690,header:#e6beab"
+export FZF_DEFAULT_OPTS="--color=fg:#e6e1e9,hl:#cbbeff \
+--color=fg+:#c9c4d0,hl+:#cbbeff \
+--color=info:#cac3dc,prompt:#cbbeff,pointer:#cbbeff \
+--color=marker:#eeb8cb,spinner:#cbbeff,header:#cac3dc"
